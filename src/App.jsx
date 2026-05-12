0de56683b19c664a5a9de501559fcd179cc27844
senerhamza <zovemsehamza@gmail.com>
2026-05-12 21:51:05 +0200

    feat: setup component architecture and connect SearchBar with API fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,55 @@
 import './App.css'
 import { useState, useEffect } from 'react'
+import Sidebar from './components/sidebar'
+
 function App() {
   const [weather, setWeather] = useState(null)
   const [city, setCity] = useState("Novi Pazar")
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
+
   const API_KEY = "783df983330ec9a2aef131fb4857f95c"
   const baseURL = "https://api.openweathermap.org/data/2.5/weather";
+
   const fetchData = async () => {
+    setLoading(true);
+    setError(null);
     try {
       const response = await fetch(`${baseURL}?q=${city}&appid=${API_KEY}&units=metric`);
       if (!response.ok) {
-        throw new Error("City not found")
+        throw new Error("Grad nije pronađen");
       }
       const data = await response.json();
-      setWeather(data)
+      setWeather(data); 
     } catch (error) {
-      setError(error.message)
+      setError(error.message);
+      setWeather(null);
     } finally {
-      setLoading(false)
+      setLoading(false); 
     }
   }
-  useEffect(()=>{
-    fetchData()
-  },[])
+
+  useEffect(() => {
+    fetchData(); 
+  }, [city]);
+
   return (
-    <>
-      <h1>Weather App</h1>
-    </>
+    <div className="app-container">
+      
+      {loading && <div className="status-message">Loading....</div>} 
+      {error && <div className="status-message error">{error}</div>} 
+
+      {weather && !loading && (
+        <div className="dashboard-layout"> 
+          <Sidebar setCity={setCity} weather={weather} /> 
+
+          <main className="main-content">
+             <h2>Today's Highlights for {weather.name}</h2>
+             <div className="temp-large">{Math.round(weather.main.temp)}°C</div>
+          </main>
+        </div>
+      )}
+    </div>
   )
 }
 
