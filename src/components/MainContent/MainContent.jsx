d281b080ef43f34375dd4aed363fbcf192cc2b9e
senerhamza <zovemsehamza@gmail.com>
2026-05-23 23:01:26 +0200

    feat(forecast): implement hourly forecast mapping with dynamic weather icons

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,39 +1,53 @@
 import { useState } from "react"
 
-const MainContent = ({weather, isCelsius, toggleUnits , forecast}) =>{
+const MainContent = ({ weather, isCelsius, toggleUnits, forecast }) => {
     const [view, setView] = useState("week");
+
+    const weekDays = ({ day }) => {
+        const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        const date = new Date(day);
+        const dayName = days[date.getDay()];
+        return dayName;
+    }
+
     return (
         <div className="main-content">
             <div className="top-bar">
                 <div className="view-bars">
-                    <button onClick={()=>{setView("today")}}>Today</button>
-                    <button onClick={()=>{setView("week")}}>Week</button>
+                    <button onClick={() => { setView("today") }}>Today</button>
+                    <button onClick={() => { setView("week") }}>Week</button>
                 </div>
                 <div className="unite-selector">
-                    <button onClick={() =>{toggleUnits(true)}}>℃</button>
-                    <button onClick={() =>{toggleUnits(false)}}>℉</button>
+                    <button onClick={() => { toggleUnits(true) }}>℃</button>
+                    <button onClick={() => { toggleUnits(false) }}>℉</button>
                 </div>
             </div>
             <div className="main">
                 {view === "today" ? (
                     <>
-                    {forecast && forecast.list && (
-                        <div className="foreacst">
-                            {forecast.list.slice(0, 5).map((item, index) => (
-                                <div className="item" key={index}>
-                                    {/* treba da lepo napravi podatke */}
-                                    <p>{item.dt_txt}</p>
-                                    <p>{isCelsius ? Math.round(item.main.temp) : Math.round((item.main.temp * 9/5) + 32)}°{isCelsius ? "C" : "F"}</p>
-                                </div>
-                            ))}
-                        </div>
-                    )}
+                        {forecast && forecast.list && (
+                            <div className="foreacst">
+                                {forecast.list.slice(0, 5).map((item, index) => (
+                                    <div className="item" key={index}>
+                                        <p>{item.dt_txt.split(" ")[1].split(":").slice(0, 2).join(":")}</p>
+                                        <img
+                                            src={`/weather-icons/${item.weather[0].icon}.png`}
+                                            alt={item.weather[0].description}
+                                            className="forecast-mini-icon"
+                                        />
+                                        <p>{isCelsius ? Math.round(item.main.temp) : Math.round((item.main.temp * 9 / 5) + 32)}°{isCelsius ? "C" : "F"}</p>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+                        {/* <p>{weekDays({ day: item.dt_txt.split(" ")[0] })}</p> */}
+
                     </>
                 ) : view === "week" ? (
                     <>
-                    <h1>h1</h1> 
+                        <h1>h1</h1>
                     </>
-                 ) : null}
+                ) : null}
             </div>
         </div>
     )
