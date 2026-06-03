e04aa01c0c31050aba9b734ac549268a8a4480c9
senerhamza <zovemsehamza@gmail.com>
2026-06-03 23:10:00 +0200

    Added sunset and sunrise

--- src/components/MainContent/MainInfo/MainInfo.jsx
+++ src/components/MainContent/MainInfo/MainInfo.jsx
@@ -13,7 +13,7 @@ const MainInfo = ({ weather }) => {
     if (diff < 0) message = "It feels colder."
 
     const sliderPosition = Math.max(10, Math.min(90, 50 + diff * 5))
-
+    console.log(weather)
     const getWindDirection = (deg) => {
         if (deg >= 337.5 || deg < 22.5) return "N";
         if (deg >= 22.5 && deg < 67.5) return "NE";
@@ -25,6 +25,12 @@ const MainInfo = ({ weather }) => {
         if (deg >= 292.5 && deg < 337.5) return "NW";
         return "N/A";
     };
+
+    const sunriseTargetTime = weather?.sys ? new Date((weather.sys.sunrise + weather.timezone) * 1000) : null;
+    const sunsetTargetTime = weather?.sys ? new Date((weather.sys.sunset + weather.timezone) * 1000) : null;
+
+    const sunriseTime = sunriseTargetTime?.toLocaleTimeString('sr-RS', { hour: '2-digit', minute: '2-digit', timeZone: 'UTC' });
+    const sunsetTime = sunsetTargetTime?.toLocaleTimeString('sr-RS', { hour: '2-digit', minute: '2-digit', timeZone: 'UTC' });
 
     return (
         <div className="insights-container">
@@ -61,7 +67,7 @@ const MainInfo = ({ weather }) => {
                             <span className="material-symbols-outlined feels-like-icon">air</span>
                             <h2>Wind</h2>
                         </div>
-                        <h1 style={{fontSize: "3rem"}} className="feels-like-temp">{Math.round(weather.wind.speed)} m/s</h1>
+                        <h1 style={{ fontSize: "3rem" }} className="feels-like-temp">{Math.round(weather.wind.speed)} m/s</h1>
                         <div className="wind-compas" >
                             <span
                                 className="material-symbols-outlined"
@@ -79,7 +85,24 @@ const MainInfo = ({ weather }) => {
                     </div>
                 )}
 
-                
+                {weather?.sys && (
+                    <div className="insights-card">
+                        <div className="Fl-icon">
+                            <span className="material-symbols-outlined feels-like-icon">wb_twilight</span>
+                            <h2>Sunrise & Sunset</h2>
+                        </div>
+                        <div className="sun-container">
+                            <div className="mini-sun-data-cont">
+                                <span className="material-symbols-outlined feels-like-icon">arrow_upward</span>
+                                <h1>{sunriseTime}</h1>
+                            </div>
+                            <div className="mini-sun-data-cont">
+                                <span className="material-symbols-outlined feels-like-icon">arrow_downward</span>
+                                <h1>{sunsetTime}</h1>
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     )
